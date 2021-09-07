14a9531f13a9a8e9eb839df528f9efbc7a6e7872
Christian Schmied <cs8898@gmx.de>
2021-09-07 09:00:05 +0200

    some Path Exports

--- mypackagelist/install-packages.sh
+++ mypackagelist/install-packages.sh
@@ -1,4 +1,12 @@
 #!/bin/bash
+
+export ANDROID_HOME=/opt/android-sdk-linux
+export ANDROID_SDK_ROOT=${ANDROID_HOME}
+export ANDROID_SDK_HOME=${ANDROID_HOME}
+export ANDROID_SDK=${ANDROID_HOME}
+
+export PATH=${PATH}:${ANDROID_HOME}/cmdline-tools/latest/bin:${ANDROID_HOME}/platform-tools:${ANDROID_HOME}/tools/bin:${ANDROID_HOME}/emulator:${ANDROID_HOME}/bin:
+
 while read p; do 
     /opt/tools/android-accept-licenses.sh "sdkmanager ${SDKMNGR_OPTS} ${p}"
 done < /opt/mypackagelist/package-list.txt
